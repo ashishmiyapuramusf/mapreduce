3b3d55c4ab81096429eb5d9d8c23777c964ffa48
ashishmiyapuramusf <126122089+ashishmiyapuramusf@users.noreply.github.com>
2023-04-01 18:13:24 -0400

    Update Reduce.py

--- Reduce.py
+++ Reduce.py
@@ -7,8 +7,11 @@ count_temp = 0
 
 
 for line in sys.stdin:
+        #Remove leading or trailing white spaces
         line = line.strip()
+        #Parse the input
         num_temp, count_temp = line.split('\t',1)
+        #Convert to int
         try:
             count_temp = int(count_temp)
             num_temp = int(num_temp)
@@ -16,8 +19,7 @@ for line in sys.stdin:
             continue
         num = num + num_temp
         count = count + count_temp
-        
-average = num / count
-print(f"Average is: {average}")
-        
-
+#Calculate average
+average = float(num) / float(count)
+#Print average
+print (average)
