77693c8c956befddc35c7b11158fcf11b25b7a51
ism6562 <ism6562@ism6562-dev-sp23-smith.myguest.virtualbox.org>
2023-04-01 17:46:02 -0400

    mapreduce average of number

--- Reduce.py
+++ Reduce.py
@@ -5,14 +5,19 @@ num_temp = 0
 count = 0
 count_temp = 0
 
+
 for line in sys.stdin:
         line = line.strip()
         num_temp, count_temp = line.split('\t',1)
-        num_temp = int(num_temp)
-        count_temp = int(count_temp)
+        try:
+            count_temp = int(count_temp)
+            num_temp = int(num_temp)
+        except ValueError:
+            continue
         num = num + num_temp
         count = count + count_temp
-
-print(num/count)
+        
+average = num / count
+print(f"Average is: {average}")
         
 
